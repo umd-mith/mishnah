5ce8179d4761fd52dbb58b0ab844894b57957918
Travis Brown <travisrobertbrown@gmail.com>
2011-12-16 12:25:23 -0500

    Removed test for ref.xml for now.

--- validation/src/test/scala/edu/umd/mith/mishnah/xml/ValidatingTest.scala
+++ validation/src/test/scala/edu/umd/mith/mishnah/xml/ValidatingTest.scala
@@ -4,6 +4,6 @@ import edu.umd.mith.util.xml.ValidatingTestBase
 
 class ValidatingTest(name: String) extends ValidatingTestBase(name) {
   val schema = "/derivative/rng/mishnah-reference.rng"
-  val docs = Seq("/tei/ref.xml")
+  val docs = Seq() //"/tei/ref.xml")
 }
 
