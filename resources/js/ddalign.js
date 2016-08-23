f0c0ca0f4e18f4521def12825fed3f2764f0d049
raffazizzi <raffaeleviglianti@gmail.com>
2016-08-23 14:13:02 -0400

    Added prev/next navigation

--- resources/js/ddalign.js
+++ resources/js/ddalign.js
@@ -21,18 +21,47 @@ var loadChapter = function (chap) {
         "alignment" : 'modules/getAlignPart.xql?ch='+chap,
         "el": "#MTV"
         }
-    ); 
+    );    
+    
 }
 
-$(window).on('hashchange',function(){ 
-    if (location.hash.slice(1)) {
-        loadChapter(location.hash.slice(1));    
+var updateNav = function (chap) {
+    // Show next/prev navigation
+    $("#dm-align-nav").show();
+    
+    var esc_chap = "ch_" + chap.replace(/\./g, "_");
+    
+    var cur_chap_nav = $("#"+esc_chap);    
+    
+    // Determine prev
+    var prev_chap = cur_chap_nav.prev('a.list-group-item');
+    if (prev_chap.length > 0) { 
+        $("#dm-align-nav a:first-child").show().attr("href", prev_chap.attr("href"));        
+    }
+    else $("#dm-align-nav a:first-child").hide();
+    
+    // Determined next
+    var next_chap = cur_chap_nav.next('a.list-group-item');
+    if (next_chap.length > 0) { 
+        $("#dm-align-nav a:last-child").show().attr("href", next_chap.attr("href"));        
+    }
+    else $("#dm-align-nav a:last-child").hide();
+}
+
+
+$(window).on('hashchange',function(){
+    var chap = location.hash.slice(1);
+    if (chap) {    
+        loadChapter(chap);   
+        updateNav(chap);
     }
 });
 
 $(window).on('load',function(){ 
-    if (location.hash.slice(1)) {
-        loadChapter(location.hash.slice(1));    
+    var chap = location.hash.slice(1)
+    if (chap) {
+        loadChapter(chap);
+        updateNav(chap);
     }
 });
    
